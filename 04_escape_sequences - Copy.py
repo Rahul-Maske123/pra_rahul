1c2d59cab7c749ccbebe5818eeab0c1f4d7873b8
Rahul <rahul.maske64@gmail.com>
2022-05-14 17:24:06 +0530

    final commit

---
+++ 04_escape_sequences - Copy.py
@@ -0,0 +1,11 @@
+story="My name is rahul.\ni am an Engineer"
+print(story)
+print("rahhhhl")
+<<<<<<< HEAD
+print("Rahul 2")
+=======
+print("Rahul")
+>>>>>>> 35fccc50e6806f878d502cef6203d48ea938511d
+print("Rahul1")
+print("rahul234")
+#copy of original
